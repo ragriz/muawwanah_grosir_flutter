8d6f2a29a26155582242e3eee8be88630adf5a65
ragriz <41234015+ragriz@users.noreply.github.com>
2023-03-09 21:51:04 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'dart:convert';
-import 'dart:io';
-import 'dart:io';
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:io' show Platform;
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:http/http.dart' as http;
 
 void main() {
@@ -26,7 +24,9 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
+List<String> list = <String>['One', 'Two', 'Three', 'Four'];
+String dropdownValue = list.first;
+var vis_login = false, vis_database = true;
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
@@ -35,14 +35,7 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-TextEditingController tf_user = TextEditingController();
-TextEditingController tf_pass = TextEditingController();
-FocusNode fn_tf_user = FocusNode();
-FocusNode fn_tf_pass = FocusNode();
-FocusNode fn = FocusNode();
-
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,98 +43,138 @@ class _MyHomePageState extends State<MyHomePage> {
           focusNode: fn,
           onKey: handleKey,
           child: Center(
-            child: Visibility(
-
-              child: Wrap(
-                children: [Container(
-                  padding: EdgeInsets.all(20),
-                  child: Card(
-                    child: Padding(
-                      padding: const EdgeInsets.all(30),
-                      child: Column(
-                        children: [
-                          ListTile(
-                            leading: Icon(Icons.person),
-                            title: Text('Muawwanah Grosir'),
-                            subtitle: Text('Silahkan Login untuk menggunakan sistem'),
-                          ),
-                          Container(
-                            margin: EdgeInsets.fromLTRB(0, 40, 0, 0),
-                            child: TextField(
-                              focusNode: fn_tf_user,
-                              controller: tf_user,
-                              textInputAction: TextInputAction.next,
-                              decoration: InputDecoration(
-                                border: UnderlineInputBorder(),
-                                labelText: 'Username',
-                              ),
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                            child: TextField(
-                              textInputAction: TextInputAction.done,
-                              controller: tf_pass,
-                              focusNode: fn_tf_pass,
-                              obscureText: true,
-                              decoration: InputDecoration(
-                                border: UnderlineInputBorder(),
-                                labelText: 'Password',
-                              ),
-                            ),
-                          ),
-                          Row(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Visibility(
+                  visible: vis_login,
+                  child: Wrap(
+                    children: [Container(
+                      padding: const EdgeInsets.all(20),
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(30),
+                          child: Column(
                             children: [
-                              Text('Hak Akses'),
-                              Padding(
-                                padding: const EdgeInsets.fromLTRB(20, 0, 0, 0),
-                                child: Expanded(
-                                  child: DropdownButton<String>(
-                                    value: 'One',
-                                    icon: const Icon(Icons.arrow_drop_down),
-                                    elevation: 5,
-                                    onChanged: (String? value) {
-                                      // This is called when the user selects an item.
-                                      setState(() {
-                                        print(value);
-                                      });
-                                    },
-                                    items: list.map<DropdownMenuItem<String>>((String value) {
-                                      return DropdownMenuItem<String>(
-                                        value: value,
-                                        child: Text(value),
-                                      );
-                                    }).toList(),
+                              const ListTile(
+                                leading: Icon(Icons.person),
+                                title: Text('Muawwanah Grosir'),
+                                subtitle: Text('Silahkan Login untuk menggunakan sistem'),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                                child: TextField(
+                                  focusNode: fn_tf_user,
+                                  controller: tf_user,
+                                  textInputAction: TextInputAction.next,
+                                  decoration: const InputDecoration(
+                                    border: UnderlineInputBorder(),
+                                    labelText: 'Username',
                                   ),
                                 ),
-                              )
+                              ),
+                              Container(
+                                margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                                child: TextField(
+                                  textInputAction: TextInputAction.done,
+                                  controller: tf_pass,
+                                  focusNode: fn_tf_pass,
+                                  obscureText: true,
+                                  decoration: const InputDecoration(
+                                    border: UnderlineInputBorder(),
+                                    labelText: 'Password',
+                                  ),
+                                ),
+                              ),
+                              Row(
+                                children: const [
+                                  Text('Hak Akses'),
+                                  Padding(
+                                    padding: EdgeInsets.fromLTRB(20, 0, 0, 0),
+                                    child: DropdownButtonExample()
+                                  )
+                                ],
+                              ),
+                              Container(
+                                margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    ElevatedButton(
+                                        onPressed: (){
+                                          login(context);
+                                        },
+                                        child: const Text('Login'))
+                                  ],
+                                ),
+                              ),
                             ],
                           ),
-                          Container(
-                            margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                ElevatedButton(
-                                    onPressed: (){
-                                      login(context);
-                                    },
-                                    child: Text('Login'))
-                              ],
-                            ),
-                          ),
-                        ],
+                        ),
                       ),
-                    ),
+                    )],
                   ),
-                )],
-              ),
+                ),
+                Visibility(
+                  visible: vis_database,
+                  child: Wrap(
+                    children: [Container(
+                      padding: const EdgeInsets.all(20),
+                      child: Card(
+                        child: Padding(
+                          padding: const EdgeInsets.all(30),
+                          child: Column(
+                            children: [
+                              const ListTile(
+                                leading: Icon(Icons.cloud),
+                                title: Text('Muawwanah Grosir'),
+                                subtitle: Text('Silahkan Pilih Database'),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.fromLTRB(0, 40, 0, 0),
+                                child: TextField(
+                                  focusNode: fn_tf_user,
+                                  controller: tf_user,
+                                  textInputAction: TextInputAction.next,
+                                  decoration: const InputDecoration(
+                                    border: UnderlineInputBorder(),
+                                    labelText: 'Alamat Database',
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    ElevatedButton(
+                                        onPressed: (){
+
+                                        },
+                                        child: const Text('Pilih'))
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    )],
+                  ),
+                )
+              ],
             ),
           ),
         )
     );
   }
 }
+
+TextEditingController tf_user = TextEditingController();
+TextEditingController tf_pass = TextEditingController();
+FocusNode fn_tf_user = FocusNode();
+FocusNode fn_tf_pass = FocusNode();
+FocusNode fn = FocusNode();
 
 login(BuildContext context) async {
   final response = await http.post(
@@ -176,26 +209,63 @@ login(BuildContext context) async {
 
 handleKey(RawKeyEvent key) {
   if (key.runtimeType.toString() == 'RawKeyDownEvent') {
-    RawKeyEventDataWeb data = key.data as RawKeyEventDataWeb;
-    //print(data.keyCode);
-    if (fn_tf_user.hasFocus) { //user focus
-      if (data.keyCode == 40) {
-        fn_tf_pass.requestFocus();
-        //var curPos = tf_user.selection.base.offset;
-        //tf_user.selection = TextSelection(baseOffset: curPos, extentOffset: curPos);
-      }
-    }
-    if (fn_tf_pass.hasFocus) { //pass focus
-      if (data.keyCode == 38) {
-        fn_tf_user.requestFocus();
-      }
-      if (data.keyCode == 40) {
+    if( Platform.isWindows ){
 
+    }
+    if( kIsWeb ){
+      RawKeyEventDataWeb data = key.data as RawKeyEventDataWeb;
+      //print(data.keyCode);
+      if (fn_tf_user.hasFocus) { //user focus
+        if (data.keyCode == 40) {
+          fn_tf_pass.requestFocus();
+          //var curPos = tf_user.selection.base.offset;
+          //tf_user.selection = TextSelection(baseOffset: curPos, extentOffset: curPos);
+        }
+      }
+      if (fn_tf_pass.hasFocus) { //pass focus
+        if (data.keyCode == 38) {
+          fn_tf_user.requestFocus();
+        }
+        if (data.keyCode == 40) {
+
+        }
       }
     }
   }
 }
-const List<String> list = <String>['One', 'Two', 'Three', 'Four'];
+
+class DropdownButtonExample extends StatefulWidget {
+  const DropdownButtonExample({super.key});
+
+  @override
+  State<DropdownButtonExample> createState() => _DropdownButtonExampleState();
+}
+
+class _DropdownButtonExampleState extends State<DropdownButtonExample> {
+  String dropdownValue = list.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_drop_down),
+      elevation: 5,
+      onChanged: (String? value) {
+        // This is called when the user selects an item.
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
+      items: list.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
+    );
+  }
+}
+
 
 class SecondRoute extends StatelessWidget {
   const SecondRoute({super.key});
